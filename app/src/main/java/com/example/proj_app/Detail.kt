24aed058efd62d5ebac69a022f5ccaa997e88446
Tt <tt_peerapat@hotmail.com>
2024-02-06 23:16:34 +0700

    add 1

--- app/src/main/java/com/example/proj_app/Detail.kt
+++ app/src/main/java/com/example/proj_app/Detail.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Detail : AppCompatActivity() {
-    /*ahhhhhhhhhhhhhh*/
+    /*ahhhhhhhhhhhhhhk*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
