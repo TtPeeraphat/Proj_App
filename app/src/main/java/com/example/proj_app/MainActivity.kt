d556875477ca053a0a3a095dc1d092b140acdfc9
gawe7 <game18130@gmail.com>
2024-02-06 23:34:16 +0700

    add 1

--- app/src/main/java/com/example/proj_app/MainActivity.kt
+++ app/src/main/java/com/example/proj_app/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.proj_app.databinding.ActivityMainBinding
-//fasdwqer qdcssf df q
+//fasdwqer qdcssf df qrg
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
