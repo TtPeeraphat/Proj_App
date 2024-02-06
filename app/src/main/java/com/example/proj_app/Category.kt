711a0387d87bdb2f87e45b88cf9fdda6e45db610
Tt <tt_peerapat@hotmail.com>
2024-02-06 23:14:36 +0700

    add 1

--- app/src/main/java/com/example/proj_app/Category.kt
+++ app/src/main/java/com/example/proj_app/Category.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.proj_app.R
 
 class Category : AppCompatActivity() {
+    /*ahhhhhhhhhhhhhh*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
