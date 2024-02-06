bd6d2168009396fe49c2e01a4dc466176d3d2979
Tt <tt_peerapat@hotmail.com>
2024-02-06 23:04:59 +0700

    Amen

---
+++ app/src/main/java/com/example/proj_app/Category.kt
@@ -0,0 +1,12 @@
+package com.example.proj_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proj_app.R
+
+class Category : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
