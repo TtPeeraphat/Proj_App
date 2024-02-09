679b5d5611e922b7599b4def8dd83ff3adb7f9e4
Tt <tt_peerapat@hotmail.com>
2024-02-09 20:45:52 +0700

    add login

--- app/src/main/java/com/example/proj_app/Detail.kt
+++ app/src/main/java/com/example/proj_app/Detail.kt
@@ -4,8 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Detail : AppCompatActivity() {
-    /*ahhhhhhhhhhhhhhdgk*/
-    //qqqqqqqqqqqqqqqq
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
