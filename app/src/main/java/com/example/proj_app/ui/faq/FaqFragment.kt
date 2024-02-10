fa70684f293f9fffe1d0db3dd0dfa999f254d810
Tt <tt_peerapat@hotmail.com>
2024-02-11 01:07:16 +0700

    add page

--- app/src/main/java/com/example/proj_app/ui/faq/FaqFragment.kt
+++ app/src/main/java/com/example/proj_app/ui/faq/FaqFragment.kt
@@ -1,4 +1,4 @@
-package com.example.proj_app.ui.dashboard
+package com.example.proj_app.ui.faq
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.proj_app.databinding.FragmentDashboardBinding
+import com.example.proj_app.databinding.FragmentFaqBinding
 
-class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+class FaqFragment : Fragment() {
+
+    private var _binding: FragmentFaqBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFaqBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/proj_app/ui/ui/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/example/proj_app/ui/ui/bookmark/BookmarkViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.proj_app.ui.ui.notifications
+package com.example.proj_app.ui.ui.bookmark
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class BookmarkViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
